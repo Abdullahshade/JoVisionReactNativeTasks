baee7f4f071216bb28c8a118471092095ccbc021
ABDALLAH ALABDELAZIZ <129231021+Abdullahshade@users.noreply.github.com>
2024-06-14 11:28:38 +0300

    Update

---
+++ Tasks/Task16.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default class Task16 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showName: false
+    };
+  }
+//rkth
+  toggleShowName = () => {
+    this.setState(prevState => ({
+      showName: !prevState.showName
+    }));
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title={this.state.showName ? 'Hide' : 'Show'}
+          onPress={this.toggleShowName}
+        />
+        {this.state.showName && (
+          <Text style={styles.headerText}>Abdullah</Text>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold'
+  },
+});
