2cd691ba4aa1e86ceaf48e37051eb0ffcbde29b9
ABDALLAH ALABDELAZIZ <129231021+Abdullahshade@users.noreply.github.com>
2024-07-08 10:30:03 +0300

    Add files via upload

---
+++ Components/Task19Component.js
@@ -0,0 +1,23 @@
+import React,{Component} from "react"
+import { View, Text, StyleSheet } from 'react-native';
+
+class MyClassPageComponent extends Component {
+    
+
+  
+
+    render () {
+
+        return (
+            
+                 <Text>Welcome To my class page~</Text>
+           
+            
+           
+        )
+    };
+
+   
+};
+
+export default MyClassPageComponent
---
+++ Task19.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import MyClassPage from './Components/Task19Component';
+
+export default class Task19 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPage: false,
+    };
+  }
+
+  toggleMyClassPage = () => {
+    this.setState((prevState) => ({
+      showPage: !prevState.showPage,
+    }));
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title={this.state.showPage ? 'Hide' : 'Show'}
+          onPress={this.toggleMyClassPage}
+        />
+        {this.state.showPage && <MyClassPage />}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
