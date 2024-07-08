a91c0b3c71a8075ffa80f4933494370f75e50005
ABDALLAH ALABDELAZIZ <129231021+Abdullahshade@users.noreply.github.com>
2024-07-08 10:33:31 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Task16 from './Tasks/Task16';
 import Task17 from './Tasks/Task17';
 import Task18 from './Tasks/Task18';
+import Task19 from './Tasks/Task19';
 
 const App = () => {
   return (
-    <Task18/>
+    <Task19/>
     
   );
 };
